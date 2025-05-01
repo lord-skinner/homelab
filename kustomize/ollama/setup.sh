5c95aaab1102374bce637aad1444dc730b965ed4
Matthew Skinner <matthew@skinnerdev.com>
2025-05-01 15:42:21 -0500

    .

---
+++ kustomize/ollama/setup.sh
@@ -0,0 +1,6 @@
+# setup nvidia gpu operator
+helm repo add nvidia https://helm.ngc.nvidia.com/nvidia
+helm repo update
+helm install --wait --generate-name \
+  -n gpu-operator --create-namespace \
+  nvidia/gpu-operator

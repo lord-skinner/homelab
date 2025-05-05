74f55f7954429b20461e1309e0a794246ef4afec
Matthew Skinner <matthew@skinnerdev.com>
2025-05-05 16:28:44 -0500

    deploy minecraft

---
+++ kustomize/minecraft/setup.sh
@@ -0,0 +1,3 @@
+kubectl create namespace minecraft
+# helm repo add itzg https://itzg.github.io/minecraft-server-charts/
+# helm install minecraft itzg/minecraft --namespace minecraft -f <your-values.yaml>

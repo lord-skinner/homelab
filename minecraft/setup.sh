5d9e072ba6e0c49db57ace20ce0a32b96e1cada0
lord-skinner <matthew@skinnerdev.com>
2025-05-18 13:44:11 -0500

    Remove deprecated Minecraft and Prismarine Kubernetes resources and configurations

--- minecraft/setup.sh
+++
@@ -1,3 +0,0 @@
-kubectl create namespace minecraft
-# helm repo add itzg https://itzg.github.io/minecraft-server-charts/
-# helm install minecraft itzg/minecraft --namespace minecraft -f <your-values.yaml>

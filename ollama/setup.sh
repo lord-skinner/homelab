392222ec2f16f56872d6e5fdb77772a22857f7d4
lord-skinner <matthew@skinnerdev.com>
2025-06-06 20:52:54 -0500

    updating examples

--- ollama/setup.sh
+++
@@ -1,6 +0,0 @@
-# setup nvidia gpu operator
-helm repo add nvidia https://helm.ngc.nvidia.com/nvidia
-helm repo update
-helm install --wait --generate-name \
-  -n gpu-operator --create-namespace \
-  nvidia/gpu-operator

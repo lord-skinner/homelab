dae75d52ac61883362837934416709041b3a8ad0
lord-skinner <matthew@skinnerdev.com>
2025-06-11 21:54:20 -0500

    .

---
+++ provisioning/testing/setup-tfpt.sh
@@ -0,0 +1,2 @@
+# in this file I want a script that sets up a TFTP server to host boot and cloud-init files for PXE network booting of stateless machines.
+# The server the TFPT Server/PXE boot environment will be set up on an ARM based device, but AMD devices will be booting from it.

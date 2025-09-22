4b1afa4cdd989e1835a060b2a89c765a545d1281
Matthew Skinner <skinner@Matthews-MacBook-Pro.local>
2025-09-22 11:42:29 -0500

    updating things

--- provisioning/testing/provision-nfs.sh
+++
@@ -1,2 +0,0 @@
-# This script will provision an NFS server for hosting the root filesystem for machines booting over PXE.
-# The server environment that NFS will be set up on is an ARM based device, but AMD devices will be booting from it.
--- provisioning/worker-nodes/README.md
+++
@@ -1,1 +0,0 @@
-<!-- Kubespray for automatic provisioning of worker nodes by the control plane nodes -->
